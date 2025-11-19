693fac7f1c89cd9d72cc82230558bb49815516f8
EdouardVtr <versterren.edouard@keemail.me>
2025-11-19 09:46:54 +0100

    Create Exercise_01_f.java

---
+++ Exercise_01_f.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int largeur = 5;
+        int hauteur = 5;
+        int count = 5;
+
+        for (int colonne = 0; colonne < hauteur; ++colonne) {
+            for (int ligne = 0; ligne < largeur; ++ligne) {
+                if (ligne < count) {
+                    System.out.print("X");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            --count;
+            System.out.println();
+        }
+    }
+}
