78ecfa674f1c009ea90d7a65b0ff72375687fa5b
EdouardVtr <versterren.edouard@keemail.me>
2025-11-19 09:37:26 +0100

    Create Exercise_01_f.java

---
+++ Exercise_01_f.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int largeur = 5;
+        int hauteur = 5;
+        
+        for (int colonne = 0; colonne < hauteur; ++colonne) {
+            for (int ligne = 0; ligne < largeur; ++ligne) {
+                if (ligne >= colonne) {
+                    System.out.print("X");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
