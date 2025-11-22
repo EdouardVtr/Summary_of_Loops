f6b03f05d4f790f9e4a93f0e363f4c4d4fcee9b2
EdouardVtr <versterren.edouard@keemail.me>
2025-11-22 18:47:23 +0100

    Update Exercise_02.java

--- Exercise_02.java
+++ Exercise_02.java
@@ -27,3 +27,33 @@ public class Main {
         }
     }
 }
+
+// Même exercice avec un do_while :
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int max, fib1 = 1, fib2 = 1, somme = 0, rep = 1;
+
+        System.out.print("Entrez votre nombre : ");
+        max = scan.nextInt();
+
+        if (max <= 1) {
+            System.out.println("Le nombre est invalide !");
+        }
+        else {
+            do {
+                somme = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = somme;
+                if (somme < max) {
+                    rep = somme;
+                }
+            } while (somme < max);
+            System.out.println(rep);
+        }
+    }
+}
